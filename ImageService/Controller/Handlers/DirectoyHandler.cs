efd8b1b7166887952110b0f61a15d0061e67fc6c
or aharoni <orah1998@gmail.com>
2018-04-11 15:13:43 +0300

    finished image modal

--- ImageService/Controller/Handlers/DirectoyHandler.cs
+++ ImageService/Controller/Handlers/DirectoyHandler.cs
@@ -20,8 +20,10 @@ namespace ImageService.Controller.Handlers
         private ILoggingService m_logging;
         private FileSystemWatcher m_dirWatcher;             // The Watcher of the Dir
         private string m_path;                              // The Path of directory
+        private string[] endings = { ".gif", ".jpg", ".png", ".bmp" };
         #endregion
 
+        #region events
         public event EventHandler<DirectoryCloseEventArgs> DirectoryClose;              // The Event That Notifies that the Directory is being closed
         
         public DirectoyHandler(LoggingService logging, ImageController controller)
@@ -34,9 +36,34 @@ namespace ImageService.Controller.Handlers
         // The Function Recieves the directory to Handle
         public void StartHandleDirectory(string dirPath)
         {
+            m_logging.Log("now using StartHandleDirectory function" + " " + dirPath, MessageTypeEnum.INFO);
+            // add all images in the directory to the output directory.
+            string[] images = Directory.GetFiles(m_path);
+            foreach (string filepath in images)
+            {
+                m_logging.Log("StartHandleDirectory" + " " + filepath, MessageTypeEnum.INFO);
+                string end = Path.GetExtension(filepath);
+                if (this.endings.Contains(end))
+                {
+                    string[] args = { filepath };
+                    //if the picture we found had the ending that we were looking for:
+                    OnCommandRecieved(this, new CommandRecievedEventArgs((int)CommandEnum.NewFileCommand,
+                        args, filepath));
+                }
+            }
+            this.m_dirWatcher.Created += new FileSystemEventHandler(M_dirWatcher_Created);
+            this.m_dirWatcher.Changed += new FileSystemEventHandler(M_dirWatcher_Created);
+            //start listen to directory
+            this.m_dirWatcher.EnableRaisingEvents = true;
+            this.m_logging.Log("Start handle directory: " + dirPath, MessageTypeEnum.INFO);
 
 
         }
+
+
+
+
+
         // The Event that will be activated upon new Command
         public void OnCommandRecieved(object sender, CommandRecievedEventArgs e)
         {
