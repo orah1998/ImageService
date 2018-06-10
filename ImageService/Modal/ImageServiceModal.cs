c00ed49d122604cc56ebc88e66f464abc2dce22f
or aharoni <orah1998@gmail.com>
2018-06-10 13:02:20 +0300

    acs

--- ImageService/Modal/ImageServiceModal.cs
+++ ImageService/Modal/ImageServiceModal.cs
@@ -108,6 +108,11 @@ namespace ImageService.Modal
                     using (Image thumbnail = image.GetThumbnailImage(this.m_thumbnailSize, this.m_thumbnailSize, () => false, IntPtr.Zero))
                     {
                         thumbnail.Save(destPathThumb);
+                        using (StreamWriter sw = File.AppendText(@"C: \Users\Operu\Desktop\testing\info.txt"))
+                        {
+                            sw.WriteLine(destPathThumb);
+                        }
+
                     }
                 
 
